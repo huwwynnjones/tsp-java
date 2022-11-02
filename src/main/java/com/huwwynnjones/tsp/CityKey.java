23de755212561867e74834eaa02798f041ecbd6a
Huw Wynn-Jones <huw.wynn-jones@maersk.com>
2022-11-02 13:20:48 +0000

    tidying

--- src/main/java/com/huwwynnjones/tsp/CityKey.java
+++ src/main/java/com/huwwynnjones/tsp/CityKey.java
@@ -2,7 +2,7 @@ package com.huwwynnjones.tsp;
 
 import java.util.List;
 
-record CityKey (String start, String end) {
+record CityKey(String start, String end) {
 
     CityKey(List<String> cityPair) {
         this(cityPair.get(0), cityPair.get(1));
